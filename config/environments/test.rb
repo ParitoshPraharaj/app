e1d908f7333e37bc89264684549c76f39afc813d
Paritosh Praharaj <parirakesh@gmail.com>
2012-04-20 11:10:58 +0530

    User Sign Up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ App::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  #Fast up the testing process by lowering Bcrypt's cost value
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST=BCrypt::Engine::MIN_COST
+  end
 end
